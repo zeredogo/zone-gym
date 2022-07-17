bd47b530b1306abd6eea04687247466972e3c2b9
zeredogo <zeredogo@gmail.com>
2022-07-17 18:15:12 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import ExerciseDetail from './components/ExerciseDetail';
+import Footer from './components/Footer';
 
 import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width='400px' sx={{width:{ xl: '1488px' }}} m='auto'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
