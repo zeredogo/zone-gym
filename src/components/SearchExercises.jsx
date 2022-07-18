0f634b736048d4f0de88a5d8c668c0d9e25b5ae8
zeredogo <zeredogo@gmail.com>
2022-07-18 22:28:47 +0100

    more commit

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px"
+    justifyContent="center" p="20px">
+      <Typography fontWeight={700} sx={{
+        fontSize: { lg: '44px', xs: '30px' }
+      }} mb="50px" textAlign='center'>
+        Awesome Exercises You <br/>
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+        height="76px"
+        value=""
+        onChange={(e) => {}}
+        placeholder="Search Exercises"
+        type="text"
+        />
+      </Box>
+    </Stack>
   )
 }
 
