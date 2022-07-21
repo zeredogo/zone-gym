1a7f6b9dc05056a7682be442509d6d4cab68e220
zeredogo <zeredogo@gmail.com>
2022-07-21 23:35:31 +0100

    more commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                 >Home</Link>
             <a href='#exercises' style={{
                 textDecoration: 'none', color:
-                '#3A1212'}}>Exercises</a>
+                '#3A1212'}}>Exercise</a>
         </Stack>
     </Stack>
   )
