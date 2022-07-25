912416675f3f0a5083ec34f0e4f1dde01130dd3f
zeredogo <zeredogo@gmail.com>
2022-07-25 20:26:11 +0100

    updating commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <h1>Krypto Zone all right reserve </h1>
+      <h1>Krypto Zone all right reserve. </h1>
     </div>
   )
 }
