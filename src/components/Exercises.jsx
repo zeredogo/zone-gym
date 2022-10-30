813aec1a00316fa425bcf7a2b4eba1514119e418
zeredogo <zeredogo@gmail.com>
2022-10-29 20:24:41 -0400

    update made

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Exercises = () => {
   return (
     <div>
-      <h1>Hello  from Paul Sunday Dogo</h1>
+      <h1>Paul Sunday Dogo</h1>
+      <p>what is your pland </p>
     </div>
   )
 }
