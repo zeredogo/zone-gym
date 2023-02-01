3a49071e0fb2c112ffd8d4f80e36b369a63fe1f2
zeredogo <zeredogo@gmail.com>
2023-02-01 13:00:21 +0100

    update made

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -7,7 +7,7 @@ const Exercises = () => {
       <p>what is your plan for the future,
          or do i have and issue organizing my life
          whats am i  suppose to  do in this clear
-         kind of situation but how cant i do 
+         kind of situation but how cant i do nice one
          it is fine every thing will work out accordingly
       </p>
       <small>Make up your Mind and work on what
