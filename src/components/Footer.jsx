a835a43c30e24947c488393a41cb07569b071e9a
zeredogo <zeredogo@gmail.com>
2023-02-02 02:28:30 +1400

    update made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <h1>Krypto Zone all right reserve. 2022 </h1>
+      <h1>
+        Krypto Zone all right reserve. 2022  with 
+        ever work for me or what should i do 
+      </h1>
     </div>
   )
 }
