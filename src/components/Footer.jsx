7cce50e9878ed7bdbf8bbc093956c67ba3560c08
zeredogo <zeredogo@gmail.com>
2023-02-02 02:44:46 +1400

    update made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ const Footer = () => {
       <h1>
         Krypto Zone all right reserve. 2022  with 
         ever work for me or what should i do in 
-        this situation now abi 
+        this situation now abi a clue will come 
+        by God grace.
       </h1>
     </div>
   )
