303981e68b691c5b7c51dc755197386c6cf7f3e7
zeredogo <zeredogo@gmail.com>
2023-02-13 13:44:28 +0100

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,6 +20,7 @@ const Footer = () => {
           man kind goes where you are will i do this things now 
           but may the Lord  help me with this hot link kilfa-li
           dose yield  loose guard university school just return 
+          till wield made here
         </Hostle>
       </p>
       <div>
