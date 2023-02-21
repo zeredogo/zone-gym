5963814453506905878b9e6e7c3d3c9e66f9a275
zeredogo <zeredogo@gmail.com>
2023-02-21 13:47:24 +0200

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,8 @@ const Footer = () => {
           the space those others refuse to fill and because she is new i will 
           do my best to enjoy every drop of my sweat in her
           i throughly  enjoy my self that day it has been 
-          a long time since i had this king of pleasure 
+          a long time since i had this king of pleasure or 
+          have a good time 
         </p>
         <div>
           <p>
