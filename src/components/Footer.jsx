2ed15c74d4d878b3a018d61bc47824520120d35e
zeredogo <zeredogo@gmail.com>
2023-02-21 13:45:56 +0200

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
           the space those others refuse to fill and because she is new i will 
           do my best to enjoy every drop of my sweat in her
           i throughly  enjoy my self that day it has been 
-          a run
+          a long time 
         </p>
         <div>
           <p>
