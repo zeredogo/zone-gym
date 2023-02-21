c934052103bc701d64246413525ed274befd657b
zeredogo <zeredogo@gmail.com>
2023-02-20 23:40:42 -1200

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           deliever me from always thinking of nacking because she will fill in 
           the space those others refuse to fill and because she is new i will 
           do my best to enjoy every drop of my sweat in her
-          i throughly 
+          i throughly  enjoy my self 
         </p>
         <div>
           <p>
