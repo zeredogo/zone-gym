23a8727b7faabda177600775fc1cea1dab255941
zeredogo <zeredogo@gmail.com>
2023-03-02 23:49:33 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,7 +86,8 @@ const Footer = () => {
           to achieve my goal, have what you want tor i am just
           nice throughly okay now is like what save what do 
           nonsense tor what should i do is like i enjoy doing 
-          this i need to do better is 
+          this i need to do better is like i enjoy doing this 
+          Abi 
         </p>
       </div>
     
