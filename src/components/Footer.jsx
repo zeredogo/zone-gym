b7b60cdb25e8e3d4d17b1031fc900c7a15198ffe
zeredogo <zeredogo@gmail.com>
2023-03-01 21:48:06 -1200

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -86,7 +86,7 @@ const Footer = () => {
           to achieve my goal, have what you want tor i am just
           nice throughly okay now is like what save what do 
           nonsense tor what should i do is like i enjoy doing 
-          this
+          this i need to 
         </p>
       </div>
     
