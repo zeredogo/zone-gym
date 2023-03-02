6d0ee81cba8ce2589babf8361ba6141d55accbd1
zeredogo <zeredogo@gmail.com>
2023-03-01 21:46:42 -1200

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -83,8 +83,8 @@ const Footer = () => {
           on this sha i dont know what to do  next but i am 
           slow my self down is this what i want  tor i don't even
           know what i want tor no wahala i have to work harder 
-          to achieve my goal, have what you want tor 
-          nice throughly okay now is like what save what
+          to achieve my goal, have what you want tor i am just
+          nice throughly okay now is like what save what do 
         </p>
       </div>
     
