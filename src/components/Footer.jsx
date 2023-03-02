6675bcd225836be7237f8f25578a4f060ccd74ae
zeredogo <zeredogo@gmail.com>
2023-03-03 07:27:37 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -88,7 +88,7 @@ const Footer = () => {
           nonsense tor what should i do is like i enjoy doing 
           this i need to do better is like i enjoy doing this 
           Abi hmm okay now anything you said what i'm i saying
-          abi suffer no the tired me no the 
+          abi suffer no the tired me e no the tired you  
         </p>
       </div>
     
