b71a3f31adf123ea76d79875422926bcd4ddb67e
zeredogo <zeredogo@gmail.com>
2023-03-04 13:29:51 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -89,7 +89,8 @@ const Footer = () => {
           this i need to do better is like i enjoy doing this 
           Abi hmm okay now anything you said what i'm i saying
           abi suffer no the tired me e no the tired you  sha 
-          i will have to do better next time due season
+          i will have to do better next time due season winter
+          rain
         </p>
       </div>
     
