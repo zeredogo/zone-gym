7566d1ed23bb310e5d511fbd051424c8893d7fb5
zeredogo <zeredogo@gmail.com>
2023-03-07 15:13:47 -0800

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -104,7 +104,7 @@ const Footer = () => {
           would have escalate more than the way it is now but 
           Thank you Lord.
           you are indeed a Great GOD.
-          yes you are indeed a 
+          yes you are indeed a God you have clear 
         </p>
       </div>
     
