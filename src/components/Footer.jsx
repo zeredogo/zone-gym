cd9d2cd19004fe54e71a05fcfc04e70304041ba3
zeredogo <zeredogo@gmail.com>
2023-03-07 15:10:03 -0800

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -104,6 +104,7 @@ const Footer = () => {
           would have escalate more than the way it is now but 
           Thank you Lord.
           you are indeed a Great GOD.
+          yes 
         </p>
       </div>
     
