2adb3f91690b7cc04af33eada59f1aae9ea49939
zeredogo <zeredogo@gmail.com>
2023-03-15 12:20:28 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -111,7 +111,7 @@ const Footer = () => {
         </p>
         <p>
           Paul Zere Sunday Adu Dogo yes continue what i think is good 
-          but the true is that 
+          but the true is that i do'
         </p>
       </div>
     
