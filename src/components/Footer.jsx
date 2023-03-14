d66275f98472c070ddd255b74fd8b24f4258a967
zeredogo <zeredogo@gmail.com>
2023-03-14 23:13:56 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -110,7 +110,7 @@ const Footer = () => {
           Lord i need you.
         </p>
         <p>
-          Paul Zere Sunday Adu Dogo yes continue 
+          Paul Zere Sunday Adu Dogo yes continue doing
         </p>
       </div>
     
