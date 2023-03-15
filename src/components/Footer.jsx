29a36819e9f4d03eb3f8e81daa0c8791e8d57385
zeredogo <zeredogo@gmail.com>
2023-03-16 13:59:42 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -112,7 +112,7 @@ const Footer = () => {
         <p>
           Paul Zere Sunday Adu Dogo yes continue what i think is good 
           but the true is that i don't know what to do next time i need 
-          to work my self up
+          to work my self up like very hard
         </p>
       </div>
     
