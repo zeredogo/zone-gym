4bf1743a65d000fd49968d4a5d30499d45726468
zeredogo <zeredogo@gmail.com>
2023-03-16 13:57:15 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -111,7 +111,7 @@ const Footer = () => {
         </p>
         <p>
           Paul Zere Sunday Adu Dogo yes continue what i think is good 
-          but the true is that i don't know what to do next
+          but the true is that i don't know what to do next time 
         </p>
       </div>
     
