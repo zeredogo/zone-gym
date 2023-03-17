b707c334892f542f0b242121999d228b37f861e6
zeredogo <zeredogo@gmail.com>
2023-03-18 10:27:44 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -114,7 +114,8 @@ const Footer = () => {
           but the true is that i don't know what to do next time i need 
           to work my self up like very hard asin very hard and i need 
           to set objective in my life and i should work very hard to
-          acheive it this should not be my life 
+          acheive it this should not be my life i can't even buy anything 
+          in my name 
         </p>
       </div>
     
