d4780f6af1913a0df53430e4191dd65bdd49b1d5
zeredogo <zeredogo@gmail.com>
2023-03-18 10:45:04 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -116,7 +116,8 @@ const Footer = () => {
           to set objective in my life and i should work very hard to
           acheive it this should not be my life i can't even buy anything 
           in my name i have differen ways to ean more money i need too 
-          work and reach my goal Lord i need to have a stable Life 
+          work and reach my goal Lord i need to have a stable Life i 
+          have to set marks on any thing i am doing  
         </p>
       </div>
     
