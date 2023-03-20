d9a8531fea4fe6d995a097d75c0f7ad96e7d44b1
zeredogo <zeredogo@gmail.com>
2023-03-21 11:56:39 +1400

    update commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -107,7 +107,8 @@ const Footer = () => {
           yes you are indeed a God you have clear road for me sevarary 
           and Forgiving me always and am still sining Lord ThaNK YOU Lord
           okay nice confirm Lord help me to conqua this and fill me 
-          Lord i need you. okay now i  need to work supper hard
+          Lord i need you. okay now i  need to work supper hard i have to
+          take
         </p>
         <p>
           Paul Zere Sunday Adu Dogo yes continue what i think is good 
